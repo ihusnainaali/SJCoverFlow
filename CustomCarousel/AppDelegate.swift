bc601be16c4af006d52ddc7839f28beb719db27d
Sameer <s.junaid@cpd.gov.ae>
2019-11-25 08:49:45 +0400

    Changes

--- CustomCarousel/AppDelegate.swift
+++ CustomCarousel/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  CustomCarousel
 //
-//  Created by Sameer Junaid on 11/4/19.
+//  Created by Sameer JD on 11/4/19.
 //  Copyright © 2019 CodiftaLabs. All rights reserved.
 //
 
